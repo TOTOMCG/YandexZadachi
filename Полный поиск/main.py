6c8f8ee3ee2c3c9656b3cd45fe31464c9a8d6e49
ARTEM <art-bogomolov@bk.ru>
2023-02-15 17:46:38 +0300

    Полный поиск

---
+++ Полный поиск/main.py
@@ -0,0 +1,45 @@
+import requests
+import pygame
+import sys
+import os
+import lib
+
+
+def show_map(ll_spn=None, map_type="map", add_params=None):
+    if ll_spn:
+        map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}"
+    else:
+        map_request = f"http://static-maps.yandex.ru/1.x/?l={map_type}"
+
+    if add_params:
+        map_request += "&" + add_params
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+
+    pygame.quit()
+    os.remove(map_file)
+
+
+
+args = lib.get_ll_span(' '.join(sys.argv[1:]))
+show_map(ll_spn=f'll={args[0]}&spn={args[1]}', add_params=f'pt={args[0]},pmwtl')
